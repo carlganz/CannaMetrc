e6d6047b1d7b283d703f21754ec8d56181619744
carlganz <carlganz@gmail.com>
2018-02-07 23:20:28 +0000

    fixes

--- R/patients.R
+++ R/patients.R
@@ -12,8 +12,8 @@ metrc_get_patient <- function(id) {
 #' @param license_number Facility license number
 #' @note See \url{https://api-co.metrc.com/Documentation/#Patients.get_patients_v1_active}
 metrc_get_patients_active <- function(license_number) {
-  metrc_call("GET", "patients/v1/active", license_number = license_number)
-  
+  metrc_call("GET", "patients/v1/active", license_number = license_number) %>%
+    bind_rows()
 }
 
 #' Get Patient Status
@@ -33,7 +33,7 @@ metrc_post_patients <- function(license_number, patient_license_number,
                                    recommended_smokable_quantity,
                                    actual_date) {
   metrc_call("POST", "patients/v1/add", license_number = license_number, body = data.frame(
-    LicenseNumber = license_number,
+    LicenseNumber = patient_license_number,
     LicenseEffectiveStartDate = license_effective_start_date,
     LicenseEffectiveEndDate = license_effective_end_date,
     RecommendedPlants = recommended_plants,
@@ -53,7 +53,7 @@ metrc_post_patients_update <- function(license_number, patient_license_number,
                                    recommended_smokable_quantity,
                                    actual_date) {
   metrc_call("POST", "patients/v1/update", license_number = license_number, body = data.frame(
-    LicenseNumber = license_number,
+    LicenseNumber = patient_license_number,
     NewLicenseNumber = new_license_number,
     LicenseEffectiveStartDate = license_effective_start_date,
     LicenseEffectiveEndDate = license_effective_end_date,
