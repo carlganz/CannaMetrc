2251dd8fa55d62d9da876ce200190a739dee57c3
carlganz <carlganz@gmail.com>
2018-02-07 23:20:20 +0000

    typo

--- R/lab_tests.R
+++ R/lab_tests.R
@@ -22,6 +22,6 @@ metrc_get_lab_test_types <- function() {
 #' @note See \url{https://api-co.metrc.com/Documentation/#LabTests.post_labtests_v1_record}
 
 metrc_post_lab_test <- function(license_number, results) {
-  metrc_call("POST", "labtests/v1/states", license_number = license_number, body = results)
+  metrc_call("POST", "labtests/v1/record", license_number = license_number, body = results)
   
 }
